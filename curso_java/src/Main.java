80c69f9181eaf48581937fc4924d4c98460d047f
xhiag <xhiag@hiago>
2024-04-19 22:24:14 -0300

    Primeiro Commit

---
+++ curso_java/src/Main.java
@@ -0,0 +1,31 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+
+//Faça um programa para ler dois valores inteiros,
+//e depois mostrar na tela a soma desses números com uma
+//mensagem explicativa, conforme exemplos.
+class Main {
+
+	public static void main(String[] args) {
+
+		Locale.setDefault(Locale.US);
+	    
+		Scanner sc = new Scanner(System.in);
+
+		int x = sc.nextInt();
+		int y = sc.nextInt();
+		
+		sc.close();
+		
+		int total = x + y;
+		
+		
+		System.out.println("A soma dos mesmos é: " + total);
+
+		
+
+	}
+
+}
+
