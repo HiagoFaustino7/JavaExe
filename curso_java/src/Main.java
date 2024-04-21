dfd11135f6d1a227814b655d5291b3a687cb77f7
xhiag <xhiag@hiago>
2024-04-21 12:39:29 -0300

    Começo ao 2 programa, renomeamento da pasta geral

--- curso_java/src/Main.java
+++ curso_java/src/Main.java
@@ -1,7 +1,7 @@
 import java.util.Locale;
 import java.util.Scanner;
 
-
+/*
 //Faça um programa para ler dois valores inteiros,
 //e depois mostrar na tela a soma desses números com uma
 //mensagem explicativa, conforme exemplos. 1-2
@@ -20,7 +20,6 @@ class Main {
 		
 		int total = x + z;
 		
-		//segundo commit test
 		
 		
 		System.out.println("A soma dos mesmos é: " + total);
@@ -30,4 +29,38 @@ class Main {
 	}
 
 }
+*/
+
+
+
+/*Faça um programa para ler o valor do raio de um círculo, e depois mostrar o valor da área deste círculo com quatro
+casas decimais conforme exemplos.*/
+
+
+class Main {
+
+	public static void main(String[] args) {
+
+		Locale.setDefault(Locale.US);
+	    
+		Scanner sc = new Scanner(System.in);
+
+		int x = sc.nextInt();
+		int z = sc.nextInt();
+		
+		sc.close();
+		
+		int total = x + z;
+		
+		
+		
+		System.out.println("A soma dos mesmos é: " + total);
+
+		
+
+	}
+
+}
+
+
 
