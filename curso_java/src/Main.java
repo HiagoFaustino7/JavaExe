53a12f7d3f7ec0e0fda4d91c1c33e8df93675fe7
xhiag <xhiag@hiago>
2024-04-21 21:44:05 -0300

    Exer 4 comentado, inicio ao exer 5

--- curso_java/src/Main.java
+++ curso_java/src/Main.java
@@ -111,6 +111,8 @@ NUMBER = 25
 SALARY = U$ 550.00
 
 */
+
+/*
 class Main {
 	
 	
@@ -133,3 +135,12 @@ class Main {
 		
 	}
 }
+*/
+
+/* Fazer um programa que leia três valores com ponto flutuante de dupla precisão: A, B e C. Em seguida, calcule e
+mostre:
+a) a área do triângulo retângulo que tem A por base e C por altura.
+b) a área do círculo de raio C. (pi = 3.14159)
+c) a área do trapézio que tem A e B por bases e C por altura.
+d) a área do quadrado que tem lado B.
+e) a área do retângulo que tem lados A e B. */
